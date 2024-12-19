caa992a0a584da5c6347b0e341423a6eff24edf5
Muhammed Shakir <shakir@infomover.io>
2024-12-19 11:48:04 +0530

    dynamic data displayed on the html template

--- src/main/java/com/im/training/springboot/controllers/CustomerController.java
+++ src/main/java/com/im/training/springboot/controllers/CustomerController.java
@@ -2,19 +2,26 @@ package com.im.training.springboot.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 /**
  * GET | POST | PUT | DELETE
- *
  */
 @Controller
 public class CustomerController {
 
 
     @GetMapping(path = "/customers")
-    public String handleGetCustomerView() {
+    public String handleGetCustomerView(Model model) {
+
+        String customerId = "101";
+        String customerName = "JP Morgan";
+
+        model.addAttribute("customerId", customerId);
+        model.addAttribute("customerName", customerName);
+
 
         return "customers";
 
