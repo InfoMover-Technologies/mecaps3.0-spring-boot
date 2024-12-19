180b6b30260cbc436e1f6859d93f516e206ea6c2
Muhammed Shakir <shakir@infomover.io>
2024-12-19 12:46:13 +0530

    list of customers displayed

--- src/main/java/com/im/training/springboot/controllers/CustomerController.java
+++ src/main/java/com/im/training/springboot/controllers/CustomerController.java
@@ -1,9 +1,12 @@
 package com.im.training.springboot.controllers;
 
 
+import com.im.training.springboot.model.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 
 /**
@@ -19,8 +22,16 @@ public class CustomerController {
         String customerId = "101";
         String customerName = "JP Morgan";
 
+        List<Customer> customers = List.of(new Customer(101, "JPMC"),
+                new Customer(102, "Morgan Stanley"),
+                new Customer(103, "Capgemini")
+
+        );
+//
         model.addAttribute("customerId", customerId);
         model.addAttribute("customerName", customerName);
+
+        model.addAttribute("customers", customers);
 
 
         return "customers";
---
+++ src/main/java/com/im/training/springboot/model/Customer.java
@@ -0,0 +1,16 @@
+package com.im.training.springboot.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    private int id;
+    private String name;
+
+}
