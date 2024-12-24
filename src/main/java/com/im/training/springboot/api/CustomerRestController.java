86eb961a44d6f5858f9741380ffac9ae07103418
shakir <shakir@infomover.io>
2024-12-24 17:48:13 +0530

    Implemnted the customers API

--- src/main/java/com/im/training/springboot/api/CustomerRestController.java
+++ src/main/java/com/im/training/springboot/api/CustomerRestController.java
@@ -4,11 +4,14 @@ package com.im.training.springboot.api;
 import com.im.training.springboot.model.Customer;
 import com.im.training.springboot.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(value = "*")
 @RestController
 public class CustomerRestController {
 
